85d97be3a434edc7a720a80576faf4318f9b5d66
Lisa Scheers <lisa@scheers.tech>
2026-08-20 07:28:59 +0200

    feat(default.nix): refactor to dynamically load program modules

--- flake-parts/homes/lisa@nook/default.nix
+++ flake-parts/homes/lisa@nook/default.nix
@@ -1,4 +1,15 @@
-{...}: {
-  imports = [../lisa];
+{...}: let
+  dir = ./programs;
+  entries = builtins.readDir dir;
+  modules =
+    map (name: dir + "/${name}")
+    (lib.filter
+      (name:
+        entries.${name}
+        == "regular"
+        && lib.hasSuffix ".nix" name)
+      (builtins.attrNames entries));
+in {
+  imports = [../lisa] ++ modules;
   home.homeDirectory = "/home/lisa";
 }
