95025e0fcbb581d0758fe470e0db30e0a02658dd
Lisa Scheers <lisa@scheers.tech>
2026-09-02 01:50:44 +0200

    fix(home-manager): correct property name for backup file extension

--- flake-parts/hosts/default.nix
+++ flake-parts/hosts/default.nix
@@ -55,7 +55,7 @@
             inputs.home-manager.nixosModules.home-manager
             {
               home-manager = {
-                home-manager.backupFileExtension = "before-nix-home-manager";
+                backupFileExtension = "before-nix-home-manager";
                 useGlobalPkgs = true;
                 useUserPackages = true;
                 extraSpecialArgs = baseSpecialArgs;
